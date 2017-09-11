d1c8c21d9ff1cc3f4608ce03206bbba24c7631e5
Hina Kawauchi <hina_17n1100142@nnn.ed.jp>
2017-09-11 16:57:46 +0900

    Answer #2

--- sql/01_articles.sql
+++ sql/01_articles.sql
@@ -6,5 +6,6 @@ CREATE TABLE `articles` (
   `body` TEXT,
   `user_id` INT NOT NULL,
   `created_at`INT PRIMARY KEY DATETIME NOT NULL, 
+  `updated_at`INT PRIMARY KEY DATETIME NOT NULL,
   FOREIGN KEY (`user_id`) REFERENCES users (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
